d4223e52c34c03440a793eb9d1d1f8197f2b556a
solarpark7346 <solarpark7346@gmail.com>
2021-09-25 16:23:25 +0900

    Login page

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// AppTabNavigator
+import MainScreen from './src/screens/MainScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const AppStackNavigator = createStackNavigator(
+    {
+        // Navigatior router
+        Login: { 
+            screen: MainScreen, 
+            navigationOptions: {
+            headerShown: false }
+        },
+        // End Navigatior route
+
+    },
+    
+    { initialRouteName: "Login", },
+    
+);
+
+const App = createAppContainer(AppStackNavigator);
+
+export default App;
---
+++ src/screens/MainScreen.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { View, Text } from "react-native";
+import styles from "./styles";
+
+export default class MainScreen extends Component {
+    goScreen(){
+        this.props.navigation.navigate('Home')
+    }
+
+    render() {
+        return (
+            <View>
+                <View style={styles.title}>
+                    <Text style={styles.font_M}>
+                        {"Sukjulyo-App"}
+                    </Text>
+
+                    <Text style={styles.font_L}>
+                        {"석줄요"}
+                    </Text>
+
+                    <Text style={{color: 'white'}}>
+                        {"모든것을 석줄로 요악한다.\n" }
+                    </Text>
+                </View>
+
+                <View style={styles.container}>
+                    <View style={styles.triangle}></View>
+                </View>
+
+                <View style={styles.login_view}>
+                    <Text 
+                        style={styles.button}
+                        onPress={() => this.Login_handler()} >
+                            Kakao Login
+                    </Text>
+
+                    <Text 
+                        style={styles.button}
+                        onPress={() => alert("준비중")} >
+                            약관보기
+                    </Text>
+
+                    <Text 
+                        style={{color: 'grey'}}>
+                            {"카카오톡앱을 사용하여 로그인을 해주세요! :) \n\n"}
+                    </Text>
+                </View>
+
+                <View style={styles.footer}>
+                    <Text 
+                        style={{color: 'black'}}>
+                            {"Team ARCHIVE \n\n"}
+                    </Text>
+                </View>
+            </View>
+        )
+    }
+}
+
